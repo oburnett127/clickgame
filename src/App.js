a904f591172d7bd5d8ffdd17dafb08dd04649a55
oburnett127 <oburnett127@gmail.com>
2022-04-11 14:57:17 -0500

    Made a few buttons for the main screen of the game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [points, setPoints] = react.useState(0);
+  const [perClick, setPerClick] = react.useState(1);
+  const [perSecond, setPerSecond] = react.useState(0);
+
+  const interval = setInterval(() => {
+    if (perSecond <= 0) return;
+    setPoints(points + perSecond);
+  }, 1000);
+
+  function addToPoints() {
+    setPoints(points + perClick);
+  }
+
+  function upgradeOne() {
+    if (points < 10) return;
+    setPoints(points - 10);
+    setPerClick(perClick + 1);
+  }
+
+  function upgradeTwo() {
+    if (points < 25) return;
+    setPoints(points - 25);
+    setPerSecond(perSecond + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>points: {points}</h2>
+      <button onClick={addToPoints}>Per Click: {perClick}</button>
+      <button onClick={upgradeOne}>
+        Spend 10 points and increase perClick
+      </button>
+      <button onClick={upgradeTwo}>Spend 25 pts and increase per second</button>
     </div>
   );
 }
